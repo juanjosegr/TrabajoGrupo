5be92485b6f097f6444f67e33ab3db36df9a7525
juanj <juanjosegr9@gmail.com>
2023-10-26 19:12:17 +0200

    fun imc

--- app/src/main/java/com/example/trabajogrupo/IMCActivity.kt
+++ app/src/main/java/com/example/trabajogrupo/IMCActivity.kt
@@ -4,26 +4,49 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 
 class IMCActivity: AppCompatActivity() {
 
+    private var tarjIzq: Boolean = true
+    var tarjDer: Boolean = false
+    private lateinit var tarjetaHombre: CardView
+    private lateinit var tarjetaFemina: CardView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc)
 
         btnRegresarMenu()
-
-
-
+        llamadaDeComponentes()
+        pulsacionesDeFunciones()
+        botonesVoF()
 
 
     }
 
+    private fun llamadaDeComponentes(){
+        tarjetaHombre = findViewById(R.id.tarjetaIzquierda)
+        tarjetaFemina = findViewById(R.id.tarjetaDerecha)
+    }
 
+    private fun botonesVoF(){
+        tarjIzq = !tarjIzq
+        tarjDer = !tarjDer
+    }
 
+    private fun pulsacionesDeFunciones(){
+        tarjetaHombre.setOnClickListener{
+            cambioDeColor()
+        }
+        tarjetaFemina.setOnClickListener{
+            println("tarjeta derecha")
+        }
+    }
 
+    private fun cambioDeColor(){
 
+    }
 
     private fun btnRegresarMenu(){
         val btnRegresar = findViewById<Button>(R.id.btnRegresarIMC)
