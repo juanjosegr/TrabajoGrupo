8cd7698ff123adde14a54e5b8cf05e78e7f0cf80
juanj <juanjosegr9@gmail.com>
2023-10-26 18:04:23 +0200

    Añadido los Manifest y las funciones del main

--- app/src/main/java/com/example/trabajogrupo/MainActivity.kt
+++ app/src/main/java/com/example/trabajogrupo/MainActivity.kt
@@ -12,23 +12,14 @@ class MainActivity : AppCompatActivity() {
 
         navegarCalculadoraJuanjo()
         navegarCalculadoraIMC()
+        navegarCalculadoraElian()
+        navegarCalculadoraDani()
     }
-
-    //Esta es la función para saltar entre pantallas
-    //Tambien en Project -> Manifest -> AndroidManifest.xml se tiene que añadir una linea de codigo con los activitys de nuestro proyecto
-    //<activity android:name=".MainActivityJuanJo"> </activity> ese es el mio por ejemplo.
-    //El tuyo seria algo asi <activity android:name=".MainActivityDani"> </activity>
-    //mira eso que te puede dar muchos dolors de cabeza, hace falta tener los 3 activitys ya metidos.
-    //En mi calculadora he creado la funcion para volver al menu que es igual que esta y solo tienes que cambiar la R.id... y el Intent (this, Mainactivity), despues de mainActivity rellenas el
-    //el nombre que quieras (ejemplo val intent = Intent(this, MainActivityDani::class.java)
-    //https://www.youtube.com/watch?v=vJapzH_46a8&list=PL8ie04dqq7_OcBYDpvHrcSFVoggLi3cm_&ab_channel=Programaci%C3%B3nAndroidbyAristiDevs 4:10
-    //de hay he sacado los datos.
 
     /**
      * Funcion creada para ir a una calculadora en específico.
      */
 
-    //Dani copia y pega eso y lo modificas con las calculadoras de los demás, y intenta crear algun tipo de when cuando esten las 3 a ver si lo consigues.
     fun navegarCalculadoraJuanjo(){
         val btnCalculadoraJuanjo = findViewById<Button>(R.id.calculadoraJuanjo)
         btnCalculadoraJuanjo.setOnClickListener{
@@ -45,4 +36,20 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
+    fun navegarCalculadoraElian(){
+        val btnCalculadoraImc = findViewById<Button>(R.id.)
+        btnCalculadoraImc.setOnClickListener{
+            val intent = Intent(this, ::class.java)
+            startActivity(intent)
+        }
+    }
+
+    fun navegarCalculadoraDani(){
+        val btnCalculadoraImc = findViewById<Button>(R.id.)
+        btnCalculadoraImc.setOnClickListener{
+            val intent = Intent(this, ::class.java)
+            startActivity(intent)
+        }
+    }
 }
