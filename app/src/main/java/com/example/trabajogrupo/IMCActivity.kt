fc40f68daa9507133f420a4cd47a442c57634528
Aneli64 <eliandeniz27@gmail.com>
2023-10-26 20:52:18 +0200

    casi terminado jiji

--- app/src/main/java/com/example/trabajogrupo/IMCActivity.kt
+++ app/src/main/java/com/example/trabajogrupo/IMCActivity.kt
@@ -7,16 +7,24 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.slider.RangeSlider
 
 class IMCActivity : AppCompatActivity() {
 
     private var tarjIzq: Boolean = true
-    var tarjDer: Boolean = false
+    private var tarjDer: Boolean = false
     private lateinit var tarjetaHombre: CardView
     private lateinit var tarjetaFemina: CardView
     private lateinit var barraMedida: RangeSlider
     private lateinit var alturaCM: TextView
+    private lateinit var sumarPeso: FloatingActionButton
+    private lateinit var restarPeso: FloatingActionButton
+    private lateinit var mostrarPeso: TextView
+    private lateinit var sumarEdad: FloatingActionButton
+    private lateinit var restarEdad: FloatingActionButton
+    private lateinit var mostrarEdad: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +41,12 @@ class IMCActivity : AppCompatActivity() {
         tarjetaFemina = findViewById(R.id.tarjetaDerecha)
         barraMedida = findViewById(R.id.BarraDeRango)
         alturaCM = findViewById(R.id.alturaEnCm)
+        sumarPeso = findViewById(R.id.btnDePesoMas)
+        restarPeso = findViewById(R.id.btnDePesoMenos)
+        mostrarPeso = findViewById(R.id.pesoEnKilos)
+        sumarEdad = findViewById(R.id.btnDeEdadMas)
+        restarEdad = findViewById(R.id.btnDeEdadMenos)
+        mostrarEdad = findViewById(R.id.edadEnNumero)
     }
 
     private fun botonesVoF() {
@@ -54,6 +68,30 @@ class IMCActivity : AppCompatActivity() {
 
         barraMedida.addOnChangeListener { _, value, _ ->
             alturaCM.text = value.toInt().toString()
+        }
+
+        sumarPeso.setOnClickListener {
+            var valorF = mostrarPeso.text.toString().toInt()
+            valorF++
+            mostrarPeso.text = valorF.toString()
+        }
+
+        restarPeso.setOnClickListener {
+            var valorF = mostrarPeso.text.toString().toInt()
+            valorF--
+            mostrarPeso.text = valorF.toString()
+        }
+
+        sumarEdad.setOnClickListener {
+            var valorF = mostrarEdad.text.toString().toInt()
+            valorF++
+            mostrarEdad.text = valorF.toString()
+        }
+
+        restarEdad.setOnClickListener {
+            var valorF = mostrarEdad.text.toString().toInt()
+            valorF--
+            mostrarEdad.text = valorF.toString()
         }
     }
 
