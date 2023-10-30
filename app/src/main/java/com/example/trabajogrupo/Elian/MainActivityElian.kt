80e2a4be780685ddb6eb5b078766231a1defc027
Aneli64 <eliandeniz27@gmail.com>
2023-10-30 14:49:50 +0100

    cambios calc 2

--- app/src/main/java/com/example/trabajogrupo/Elian/MainActivityElian.kt
+++ app/src/main/java/com/example/trabajogrupo/Elian/MainActivityElian.kt
@@ -55,7 +55,7 @@ class MainActivityElian : AppCompatActivity() {
                 if (resultado == ""){
                     mensError("Error de cifras")
                 }
-                texto.hint = resultado
+                texto.text = resultado
             }
         }
         btnRegresarMenu()
